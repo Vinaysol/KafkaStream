815e193ac957e31155d6cc5c8321d27f323ba4fa
vinays <vinaysolanki@hotmail.com>
2023-09-28 23:03:08 +0530

    "first"

---
+++ src/test/java/SpringBootKafka/kafkaStream/SpringBootKafkaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootKafka.kafkaStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
